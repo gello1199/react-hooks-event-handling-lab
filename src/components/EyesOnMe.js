6cebe511828011b0abfcb81e02af2266c15abf97
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-31 17:35:19 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+
+import React from 'react';
+
+const EyesOnMe = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default EyesOnMe;
+
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+
+import React from 'react';
+
+const Keypad = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Keypad;
